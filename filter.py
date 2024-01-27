dd7a2a9169893d57b73cebd9751963c1fbf222f9
Matthew <mhamilton942@gmail.com>
2024-01-27 18:10:32 +0100

    Update filter.py

--- filter.py
+++ filter.py
@@ -4,4 +4,5 @@ colfis = pd.read_csv("Forme_inverso.tsv", sep='\t')
 colfis = colfis.sort_values('Rango')
 colfis = colfis[colfis['Cat.Gram'].apply(lambda x: 'K' not in x)]
 colfis = colfis[colfis['Forma'].apply(lambda x: '-' not in x)]
-colfis.drop_duplicates(subset=['Forma'], keep='first').head(1000).to_csv("top_1000.tsv", sep='\t')
+colfis = colfis[colfis['Forma'].apply(lambda x: not x.isdigit())]
+colfis.drop_duplicates(subset=['Forma'], keep='first').head(1000)[["Forma","Freq","Cat.Gram"]].to_csv("top_1000.tsv", sep='\t', index=False)
