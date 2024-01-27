8026b8b6fc34587060b56bec9e651ca549524fe6
Matthew Hamilton <matthew.hamilton2@unibo.it>
2024-01-27 15:49:46 +0100

    Create filter.py

---
+++ filter.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+colfis = pd.read_csv("Forme_inverso.tsv", sep='\t')
+colfis = colfis.sort_values('Rango')
+colfis = colfis[colfis['Cat.Gram'].apply(lambda x: 'K' not in x)]
+colfis = colfis[colfis['Forma'].apply(lambda x: '-' not in x)]
+colfis.drop_duplicates(subset=['Forma'], keep='first').head(1000).to_csv("top_1000.tsv", sep='\t')
