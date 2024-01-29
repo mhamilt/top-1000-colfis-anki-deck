ce702dd5daf7e794931b76ddb6a7bd7475af9fcd
Matthew <mhamilton942@gmail.com>
2024-01-29 22:32:56 +0100

    Update make_deck.py

--- make_deck.py
+++ make_deck.py
@@ -1,1 +1,13 @@
+import pandas as pd
 
+def main():	
+	"""
+	- concatenate each deck
+	- remove duplicate italian words
+	- send each word to 'say' command and output aiff
+		say -v 'Emma' -o "audio/${word}.aiff" "${word}" 
+	"""
+	pass
+
+if __name__ == "__main__":
+	main()
